907de81b75fc71ca74fc2b0aad08c126115a90cc
abdlatif-nabgha <abdelatif.nabgha06@gmail.com>
2026-02-03 10:48:11 +0100

    correct the error in providers/data

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,10 @@
-import { BACKEND_BASE_URL, DEPARTMENTS, subjects } from "@/constants";
+import { BACKEND_BASE_URL } from "@/constants";
 import { ListResponse } from "@/types";
 import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
+
+if ( !BACKEND_BASE_URL) {
+  throw new Error('Missing environment variable: VITE_BACKEND_BASE_URL');
+}
 
 const options: CreateDataProviderOptions = {
   getList: {
@@ -30,11 +34,11 @@ const options: CreateDataProviderOptions = {
       return params;
      },
      mapResponse: async (response) => {
-      const payload: ListResponse = await response.json();
+      const payload: ListResponse = await response.clone().json();
       return payload.data ?? [];
      },
      getTotalCount: async (response) => {
-      const payload: ListResponse = await response.json();
+      const payload: ListResponse = await response.clone().json();
       return payload.pagination?.total ?? payload.data?.length ?? 0;
      }
   },
